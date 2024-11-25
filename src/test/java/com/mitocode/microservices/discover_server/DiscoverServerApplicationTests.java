9ec40adc23de92f2604a65c4b54ef8d4a0965990
Ruben Moreno <moreno.greenwater@gmail.com>
2024-11-25 00:01:32 -0500

    first commit

---
+++ src/test/java/com/mitocode/microservices/discover_server/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.microservices.discover_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
